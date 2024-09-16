94868612526d8993568b3a99fc6b15f59e185e84
David Malchin <malchin459@gmail.com>
2024-09-16 13:58:36 +0300

    feat(qbox): add `compat` command

--- src/components/qbox/index.ts
+++ src/components/qbox/index.ts
@@ -1,6 +1,7 @@
 import { Component } from '../types.js';
 import { aboutCommand } from './commands/about.js';
 import { builtinCommand } from './commands/builtin.js';
+import { compatCommand } from './commands/compat.js';
 import { docsCommand } from './commands/docs.js';
 import { resourceCommand } from './commands/resource.js';
 import { supportCommand } from './commands/support/chat-input-command.js';
@@ -10,6 +11,7 @@ export default {
     commands: [
         aboutCommand,
         builtinCommand,
+        compatCommand,
         docsCommand,
         resourceCommand,
         supportCommand,
