5258a47dfa02b8376f920e8b6e1853b1fffff949
Luna Kohl <hnkohl@gmail.com>
2024-12-03 19:07:35 -0800

    Oopsie, missed a couple

--- src/components/utility/index.ts
+++ src/components/utility/index.ts
@@ -12,7 +12,7 @@ export default {
         letmegooglethatCommand,
         plshelpCommand,
         plshelpMessageCommand,
-        spoonfeedCommand,
-        spoonfeedMessageCommand,
+        resourcesCommand,
+        resourcesMessageCommand,
     ],
 } satisfies Component;
