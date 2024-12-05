dd6e71a12e7b4e8c73536aa33b065f59a04ff925
Luna Kohl <hnkohl@gmail.com>
2024-12-04 16:36:49 -0800

    I hate linting.
    It looked better before, and I kept the same syntax from the original but CI is smelli and keeps bitching about it.

--- src/components/utility/commands/resources/message.ts
+++ src/components/utility/commands/resources/message.ts
@@ -9,11 +9,13 @@ import {
 
 export function resourcesMessage(userId?: Snowflake) {
     const content = [
-        'Learning programming can be both exciting and challenging, especially when you\'re starting out.',
+        "Learning programming can be both exciting and challenging, especially when you're starting out.",
         'Diving into code, exploring resources, and experimenting are essential parts of the journey.',
         'While it takes time and effort to develop your skills, we encourage curiosity and a growth mindset.',
-        'Whether you\'re an experienced programmer or new to FiveM development and Lua, we\'re here to support your learning.',
-        bold('Below are some excellent resources curated by the Qbox team to help you get started:'),
+        "Whether you're an experienced programmer or new to FiveM development and Lua, we're here to support your learning.",
+        bold(
+            'Below are some excellent resources curated by the Qbox team to help you get started:',
+        ),
         unorderedList([
             hyperlink('Lua Docs', hideLinkEmbed('https://lua.org/docs.html')),
             hyperlink(
