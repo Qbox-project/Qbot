0c3babce2653bfc14a8c1113e7d286ca7297b388
David Malchin <malchin459@gmail.com>
2025-05-11 03:30:48 +0300

    refactor(qbox/tutorial): fetch tutorials

--- src/components/qbox/commands/tutorial.ts
+++ src/components/qbox/commands/tutorial.ts
@@ -1,20 +1,42 @@
 import {
+    APIApplicationCommandInteractionDataStringOption,
     APIInteractionResponseCallbackData,
     ApplicationCommandOptionType,
     ApplicationCommandType,
 } from '@discordjs/core';
-import { hideLinkEmbed, userMention } from '@discordjs/formatters';
-import { docsUrl } from '../constants.js';
+import { hideLinkEmbed, inlineCode, userMention } from '@discordjs/formatters';
+import rss from 'rss-to-json';
+import { color, docsUrl } from '../constants.js';
 import { ChatInputCommand } from '/components/types.js';
 import { mapChatInputOptionValues } from '/utils/interactions.js';
+import { search } from '/utils/search.js';
 
-const docs: Record<string, string> = {
-    'how-fxmanifests-work': 'Learn how fxmanifests work with FiveM:',
-    'vscode-setup':
-        'Learn how to set up VSCode for FiveM and RedM Lua scripting:',
-    'install-custom-weapons':
-        'Learn how to install custom weapons in Qbox using ox_inventory:',
-};
+async function fetchFeed() {
+    const response = await rss.parse(`${docsUrl}/blog/atom.xml`);
+
+    const entries = response.items.map(
+        (x) =>
+            [
+                x.id.replace(`${docsUrl}/blog/`, ''),
+                {
+                    title: x.title,
+                    description: x.description,
+                    link: x.link,
+                    author: x.author,
+                },
+            ] as [
+                string,
+                {
+                    title: string;
+                    description: string;
+                    link: string;
+                    author: string;
+                },
+            ],
+    );
+
+    return new Map(entries);
+}
 
 export const tutorialCommand = {
     data: {
@@ -26,8 +48,7 @@ export const tutorialCommand = {
                 type: ApplicationCommandOptionType.String,
                 name: 'link',
                 description: 'The link to send',
-                choices: Object.keys(docs).map((k) => ({ name: k, value: k })),
-                required: true,
+                autocomplete: true,
             },
             {
                 type: ApplicationCommandOptionType.User,
@@ -41,19 +62,62 @@ export const tutorialCommand = {
             interaction.data,
         ) as { link: string; mention: string | undefined };
 
+        await api.interactions.defer(interaction.id, interaction.token);
+
         const response: APIInteractionResponseCallbackData = {
             content: mention ? `${userMention(mention)} ` : '',
         };
 
-        response.content! += [
-            docs[link],
-            hideLinkEmbed(`${docsUrl}/blog/${link}`),
-        ].join('\n');
+        if (!link) {
+            response.content! += `Read our tutorials here: ${docsUrl}/blog`;
+        } else {
+            const feed = await fetchFeed();
+            const entry = feed.get(link);
 
-        await api.interactions.reply(
-            interaction.id,
+            if (!entry) {
+                await api.interactions.editReply(
+                    interaction.application_id,
+                    interaction.token,
+                    { content: `No such tutorial ${inlineCode(link)}.` },
+                );
+                return;
+            }
+
+            response.content! += hideLinkEmbed(entry.link);
+            response.embeds = [
+                {
+                    title: `Tutorial: ${entry.title}`,
+                    url: entry.link,
+                    description: entry.description,
+                    color,
+                    author: { name: entry.author },
+                },
+            ];
+        }
+
+        await api.interactions.editReply(
+            interaction.application_id,
             interaction.token,
             response,
+        );
+    },
+    async autocomplete({ api, data: interaction }) {
+        const value = (
+            interaction.data
+                .options[0]! as APIApplicationCommandInteractionDataStringOption
+        ).value;
+
+        const feed = await fetchFeed();
+
+        await api.interactions.createAutocompleteResponse(
+            interaction.id,
+            interaction.token,
+            {
+                choices: search(Array.from(feed.keys()), value).map((r) => ({
+                    name: r,
+                    value: r,
+                })),
+            },
         );
     },
 } satisfies ChatInputCommand;
