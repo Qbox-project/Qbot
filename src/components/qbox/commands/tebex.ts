6d48e9958e31c5ed3b0058d81b64c0ee4f64f971
David Malchin <malchin459@gmail.com>
2025-11-25 04:01:26 +0200

    refactor(qbox): use quick reply tebex command

--- src/components/qbox/commands/tebex.ts
+++ src/components/qbox/commands/tebex.ts
@@ -1,21 +1,29 @@
-import {
-    APIInteractionResponseCallbackData,
-    ApplicationCommandOptionType,
-    ApplicationCommandType,
-} from '@discordjs/core';
-import { hideLinkEmbed, userMention } from '@discordjs/formatters';
+import { ApplicationCommandOptionType } from '@discordjs/core';
+import { hideLinkEmbed } from '@discordjs/formatters';
 import { docsUrl } from '../constants.js';
-import { ChatInputCommand } from '/components/types.js';
-import { mapChatInputOptionValues } from '/utils/interactions.js';
+import { createQuickReplyChatInputCommand } from '/utils/quick-reply.js';
 
-const docs: Record<string, string> = {
-    slots: 'Check out our Casino Slots resource on tebex:',
-    'weapon-audio': 'Check out our Weapon Audio resource on tebex:',
+const links = {
+    cdn: {
+        name: 'CDN',
+        message: 'Check out our CDN:',
+    },
+    'loki-logging': {
+        name: 'Loki Logging',
+        message: 'Check out our Loki Logging:',
+    },
+    slots: {
+        name: 'Casino Slots',
+        message: 'Check out our Casino Slots resource:',
+    },
+    'weapon-audio': {
+        name: 'Weapon Audio',
+        message: 'Check out our Weapon Audio resource:',
+    },
 };
 
-export const tebexCommand = {
+export const tebexCommand = createQuickReplyChatInputCommand({
     data: {
-        type: ApplicationCommandType.ChatInput,
         name: 'tebex',
         description: 'Send a Qbox tebex link',
         options: [
@@ -23,34 +31,20 @@ export const tebexCommand = {
                 type: ApplicationCommandOptionType.String,
                 name: 'link',
                 description: 'The link to send',
-                choices: Object.keys(docs).map((k) => ({ name: k, value: k })),
+                choices: Object.entries(links).map(([value, { name }]) => ({
+                    name,
+                    value,
+                })),
                 required: true,
-            },
-            {
-                type: ApplicationCommandOptionType.User,
-                name: 'mention',
-                description: 'An optional user to mention',
             },
         ],
     },
-    async execute({ api, data: interaction }) {
-        const { link, mention } = mapChatInputOptionValues(
-            interaction.data,
-        ) as { link: string; mention: string | undefined };
+    withOptionalMention: true,
+    message({ link }) {
+        const linkMessage = links[link as keyof typeof links];
 
-        const response: APIInteractionResponseCallbackData = {
-            content: mention ? `${userMention(mention)} ` : '',
+        return {
+            content: `${linkMessage}${hideLinkEmbed(`${docsUrl}/tebex/${link}`)}`,
         };
-
-        response.content! += [
-            docs[link],
-            hideLinkEmbed(`${docsUrl}/tebex/${link}`),
-        ].join('\n');
-
-        await api.interactions.reply(
-            interaction.id,
-            interaction.token,
-            response,
-        );
     },
-} satisfies ChatInputCommand;
+});
