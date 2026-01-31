59a60cb64b8ffd32a7aad5d45ec41541b354157e
David Malchin <malchin459@gmail.com>
2026-02-01 01:03:27 +0200

    refactor(qbox/artifacts): format

--- src/components/qbox/commands/artifacts.ts
+++ src/components/qbox/commands/artifacts.ts
@@ -1,12 +1,9 @@
-import {
-    hideLinkEmbed,
-    hyperlink
-} from '@discordjs/formatters';
+import { hideLinkEmbed, hyperlink } from '@discordjs/formatters';
 import { createQuickReplyChatInputCommand } from '/utils/quick-reply.js';
 
 const content = `We recommend using the JGScripts ${hyperlink(
     'FiveM Artifacts DB',
-    hideLinkEmbed('https://artifacts.jgscripts.com/')
+    hideLinkEmbed('https://artifacts.jgscripts.com/'),
 )} to install your artifacts.
 This website curates and reports issues found within recently released versions.`;
 
